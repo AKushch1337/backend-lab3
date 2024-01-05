983325668f707e49bbb0987808312da0a57f43b5
Artem Kushch <artemkusch2@gmail.com>
2024-01-05 09:18:47 +0200

    Add Ktor modules for Data Validation and Status Pages

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,4 +29,8 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    // Content Validation
+    implementation("io.ktor:ktor-server-request-validation:$ktor_version")
+// Server Status Pages
+    implementation("io.ktor:ktor-server-status-pages:$ktor_version")
 }
