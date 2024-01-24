aa51df20f6fe05342cbc435a288bac724d5303bd
Artem Kushch <artemkusch2@gmail.com>
2024-01-24 12:49:44 +0200

    Add defaultCurrencyCode to User entity

--- src/main/kotlin/com/example/model/User.kt
+++ src/main/kotlin/com/example/model/User.kt
@@ -6,8 +6,10 @@ import org.jetbrains.exposed.sql.Table
 data class User(
     var id: Int,
     val name: String,
+    val defaultCurrencyId: Int?
 )
 
 object Users : IntIdTable() {
     val name = varchar("name", 255)
+    val defaultCurrencyId = integer("default_currency_id").references(Currencies.id).nullable()
 }
